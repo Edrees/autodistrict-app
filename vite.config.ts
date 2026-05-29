4f6ecb704f9f24d7e78059e8e3b3a760bf38a4c4
Edrees <s.i.sarwari@hotmail.com>
2026-05-29 14:05:58 +0200

    fix: disable sourcemaps to fix CSP eval error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/autodistrict-app/',
   plugins: [react()],
+  base: '/autodistrict-app/',
+  build: {
+    sourcemap: false, // Dit voorkomt het genereren van sourcemaps die 'eval' gebruiken
+    minify: 'terser', // Optioneel: zorgt voor een schone, veilige compressie
+  },
 })
